56683418a87c447c3000947905e95d7f7173ee2d
Deepanshu Shah <112880677+deepanshushah2002@users.noreply.github.com>
2022-11-13 09:20:56 +0530

    Add files via upload

---
+++ Square_Patten.py
@@ -0,0 +1,10 @@
+  #    * * * * *
+  #    * * * * *
+  #    * * * * *
+  #    * * * * *
+  #    * * * * *
+a = int(input("Enter a Number"))
+for i in range(a):
+    for i in range(a):
+        print("*", end=(" ")) 
+    print()
