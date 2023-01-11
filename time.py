4387eae978fadb6dcc1f289816cb3786215665cc
Deepanshu Shah <112880677+deepanshushah2002@users.noreply.github.com>
2023-01-11 21:20:50 +0530

    Add files via upload

---
+++ time.py
@@ -0,0 +1,13 @@
+import time
+timestamp = time.strftime('%I:%M:%S %p')
+monthstamp = time.strftime('%d/%m/%y %a')
+print("Your Day is:-", monthstamp)
+print("Your Time is:-", timestamp)
+if (time.strftime('%H') in ['12', '13', '14', '15' ,'16']):
+    print("Hello Sir, Good Afternoon")
+elif (time.strftime('%H') in ['17','18','19']):
+    print("Hello Sir, Good Evening")
+elif (time.strftime('%H') in ['20','21','22','23','24']):
+    print("Hello Sir, Good Night")
+else:
+    print("Hello Sir, Good Morning")
